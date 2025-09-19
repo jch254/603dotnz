548c878624d8e4d6f23266f8ecc9b3163fb1dda8
Jordan Hornblow <jordan@jch254.com>
2025-09-19 15:54:46 +0700

    NEW NEW BUT DIFFERENT

--- components/Main.tsx
+++ components/Main.tsx
@@ -67,6 +67,63 @@ const Main: React.FC = () => (
         <SectionHeader mt={0} heading="Projects" />
         <Flex align="center" justify="center" wrap gutter={2}>
           <Card m={2} style={{ width: '309px', height: '333px' }}>
+            <a href="https://lushauraltreats.com">
+              <CardImage src="/img/LushAuralTreats.svg" />
+            </a>
+            <HeadingLink href="https://lushauraltreats.com">
+              Lush Aural Treats
+            </HeadingLink>
+            <Text small>
+              Fire through a music link and it quietly shows up in a lean API +
+              a featherweight web UI. Built with NestJS, DynamoDB, and vanilla TS.
+            </Text>
+          </Card>
+          <Card m={2} style={{ width: '309px', height: '333px' }}>
+            <a
+              href="https://github.com/jch254/discogs-market-monitor"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <CardImage src="/img/Wax.jpeg" />
+            </a>
+            <Heading>
+              <a
+                href="https://github.com/jch254/discogs-market-monitor"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Discogs Wantlist Monitor
+              </a>
+            </Heading>
+            <Text small>
+              Saves manually searching through your Discogs wantlist for local
+              listings
+            </Text>
+          </Card>
+          <Card m={2} style={{ width: '309px', height: '333px' }}>
+            <a href="https://starter-pack-typescript.603.nz">
+              <CardImage src="/img/SP.png" />
+            </a>
+            <Flex align="center">
+              <HeadingLink href="https://starter-pack-typescript.603.nz">
+                Starter Pack
+              </HeadingLink>
+              <Space auto />
+              <a
+                href="https://github.com/jch254/starter-pack"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                GitHub
+              </a>
+            </Flex>
+            <Text small>
+              Combines React (ft. hooks), Redux, Redux-saga and TypeScript with
+              Auth0 as a starting point for modern web apps with solid
+              authentication.
+            </Text>
+          </Card>
+          <Card m={2} style={{ width: '309px', height: '333px' }}>
             <a href="https://buildpipeline-prod.603.nz">
               <CardImage src="/img/BuildPipeline.png" />
             </a>
@@ -86,6 +143,50 @@ const Main: React.FC = () => (
             <Text small>
               BuildPipeline is an AWS-powered serverless build, test and deploy
               pipeline ft. multiple environments (test and production).
+            </Text>
+          </Card>
+          <Card m={2} style={{ width: '309px', height: '333px' }}>
+            <a href="https://serverless-api.603.nz">
+              <CardImage src="/img/SA.png" />
+            </a>
+            <Flex align="center">
+              <HeadingLink href="https://serverless-api.603.nz">
+                Serverless API
+              </HeadingLink>
+              <Space auto />
+              <a
+                href="https://github.com/jch254/serverless-node-dynamodb-api"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                GitHub
+              </a>
+            </Flex>
+            <Text small>
+              API powered by Serverless, TypeScript, Webpack and DynamoDB,
+              intended as a starting point for Serverless APIs.
+            </Text>
+          </Card>
+          <Card m={2} style={{ width: '309px', height: '333px' }}>
+            <a href="https://audio-insights.603.nz">
+              <CardImage src="/img/AI.png" />
+            </a>
+            <Flex align="center">
+              <HeadingLink href="https://audio-insights.603.nz">
+                Audio Insights
+              </HeadingLink>
+              <Space auto />
+              <a
+                href="https://github.com/jch254/audio-insights"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                GitHub
+              </a>
+            </Flex>
+            <Text small>
+              Insights, visualizations and recommendations derived from your
+              Spotify library. Powered by React, Redux and Redux-saga.
             </Text>
           </Card>
           <Card m={2} style={{ width: '309px', height: '333px' }}>
@@ -129,106 +230,6 @@ const Main: React.FC = () => (
             </Text>
           </Card>
           <Card m={2} style={{ width: '309px', height: '333px' }}>
-            <a href="https://scratchpad.zone">
-              <CardImage src="/img/Scratchpad.png" />
-            </a>
-            <HeadingLink href="https://scratchpad.zone">Scratchpad</HeadingLink>
-            <Text small>
-              Scratchpad is an organized repository for ideas, quotes,
-              anecdotes, observations and information you come across during
-              your quest through life.
-            </Text>
-          </Card>
-          <Card m={2} style={{ width: '309px', height: '333px' }}>
-            <a href="https://audio-insights.603.nz">
-              <CardImage src="/img/AI.png" />
-            </a>
-            <Flex align="center">
-              <HeadingLink href="https://audio-insights.603.nz">
-                Audio Insights
-              </HeadingLink>
-              <Space auto />
-              <a
-                href="https://github.com/jch254/audio-insights"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                GitHub
-              </a>
-            </Flex>
-            <Text small>
-              Insights, visualizations and recommendations derived from your
-              Spotify library. Powered by React, Redux and Redux-saga.
-            </Text>
-          </Card>
-          <Card m={2} style={{ width: '309px', height: '333px' }}>
-            <a href="https://starter-pack-typescript.603.nz">
-              <CardImage src="/img/SP.png" />
-            </a>
-            <Flex align="center">
-              <HeadingLink href="https://starter-pack-typescript.603.nz">
-                Starter Pack
-              </HeadingLink>
-              <Space auto />
-              <a
-                href="https://github.com/jch254/starter-pack"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                GitHub
-              </a>
-            </Flex>
-            <Text small>
-              Combines React (ft. hooks), Redux, Redux-saga and TypeScript with
-              Auth0 as a starting point for modern web apps with solid
-              authentication.
-            </Text>
-          </Card>
-          <Card m={2} style={{ width: '309px', height: '333px' }}>
-            <a href="https://serverless-api.603.nz">
-              <CardImage src="/img/SA.png" />
-            </a>
-            <Flex align="center">
-              <HeadingLink href="https://serverless-api.603.nz">
-                Serverless API
-              </HeadingLink>
-              <Space auto />
-              <a
-                href="https://github.com/jch254/serverless-node-dynamodb-api"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                GitHub
-              </a>
-            </Flex>
-            <Text small>
-              API powered by Serverless, TypeScript, Webpack and DynamoDB,
-              intended as a starting point for Serverless APIs.
-            </Text>
-          </Card>
-          <Card m={2} style={{ width: '309px', height: '333px' }}>
-            <a
-              href="https://github.com/jch254/discogs-market-monitor"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <CardImage src="/img/Wax.jpeg" />
-            </a>
-            <Heading>
-              <a
-                href="https://github.com/jch254/discogs-market-monitor"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Discogs Wantlist Monitor
-              </a>
-            </Heading>
-            <Text small>
-              Saves manually searching through your Discogs wantlist for local
-              listings
-            </Text>
-          </Card>
-          <Card m={2} style={{ width: '309px', height: '333px' }}>
             <a
               href="https://github.com/jch254/terraform-ecs-autoscale-alb"
               target="_blank"
@@ -249,6 +250,33 @@ const Main: React.FC = () => (
               AWS ECS cluster with instance and service autoscaling configured
               and running behind an ALB with path based routing set up.
             </Text>
+          </Card>
+          <Card m={2} style={{ width: '309px', height: '333px' }}>
+            <a href="https://scratchpad.zone">
+              <CardImage src="/img/Scratchpad.png" />
+            </a>
+            <HeadingLink href="https://scratchpad.zone">Scratchpad</HeadingLink>
+            <Text small>
+              Scratchpad is an organized repository for ideas, quotes,
+              anecdotes, observations and information you come across during
+              your quest through life.
+            </Text>
+          </Card>
+          <Card m={2} style={{ width: '309px', height: '333px' }}>
+            <CardImage src="/img/Matrix.gif" />
+            <Flex align="center">
+              <Heading>Under Development</Heading>
+              <Space auto />
+            </Flex>
+            <Text small>Check back soon.</Text>
+          </Card>
+          <Card m={2} style={{ width: '309px', height: '333px' }}>
+            <CardImage src="/img/Matrix.gif" />
+            <Flex align="center">
+              <Heading>Under Development</Heading>
+              <Space auto />
+            </Flex>
+            <Text small>Check back soon.</Text>
           </Card>
           <Card m={2} style={{ width: '309px', height: '333px' }}>
             <CardImage src="/img/Matrix.gif" />
