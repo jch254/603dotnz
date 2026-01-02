6d3d67f37758ccd259a7fb557428cc0c1094dbbd
Jordan Hornblow <jordan@jch254.com>
2026-01-02 14:54:54 +1300

    fix styling

--- components/Head.tsx
+++ components/Head.tsx
@@ -9,7 +9,7 @@ const Head: React.FC<HeadProps> = ({ title }) => (
     <base href="/603dotnz/" />
     <meta charSet="utf-8" />
     <title>{title}</title>
-    <link href="/index.css" rel="stylesheet" />
+    <link href="index.css" rel="stylesheet" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="shortcut icon" href="/favicon.ico" />
     {
